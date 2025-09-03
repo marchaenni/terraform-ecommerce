f228c57ed340b088f81a7cbbd4418164071502d7
marchaenni <marc.haenni@hotmail.ch>
2025-09-03 11:39:32 +0200

    Auftrag erstellt durch CHatGPT

---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# Terraform Ecommerce (AWS)
+Automatisiert die ursprüngliche Aufgabe:
+- VPC, Subnets, NAT, IGW, Routing
+- SGs (Web, DB), NACLs (allow all)
+- EC2 Web (Nginx+Gunicorn) & DB (MariaDB)
+- Route53 Private Zone (webshop.tbz)
+
+**Fixes:** keine Portkollision, sauberes Cloud-Init, Basic-Auth, DB-Zugriff nur Web.
---
+++ variables.tf
@@ -0,0 +1,18 @@
+﻿variable "aws_region" { type = string, default = "us-east-1" }
+variable "aws_az"     { type = string, default = "us-east-1a" }
+variable "vpc_cidr"            { type = string, default = "10.11.5.0/24" }
+variable "public_subnet_cidr"  { type = string, default = "10.11.5.192/26" }
+variable "private_subnet_cidr" { type = string, default = "10.11.5.0/26" }
+variable "ssh_public_key" { type = string, sensitive = true }
+variable "instance_type_web" { type = string, default = "t2.micro" }
+variable "instance_type_db"  { type = string, default = "t2.micro" }
+variable "domain_name" { type = string, default = "webshop.tbz" }
+variable "wsgi_module" { type = string, default = "application:app" }
+variable "app_zip_url" { type = string }
+variable "sql_dump_url" { type = string, default = "" }
+variable "db_name" { type = string, default = "ecommerce" }
+variable "db_user" { type = string, default = "shopuser" }
+variable "db_password" { type = string, sensitive = true }
+variable "basic_auth_user" { type = string, default = "webuser" }
+variable "basic_auth_password" { type = string, sensitive = true }
+variable "tags" { type = map(string), default = { Project="Ecommerce", IaC="Terraform" } }
